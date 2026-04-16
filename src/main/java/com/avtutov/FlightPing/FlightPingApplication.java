609880348dbb941a87415e5251084886f83d4437
Andrei Avtutov <aavtutov@gmail.com>
2026-04-16 15:16:21 +0300

    Initial commit: Spring Boot 3.5.13 project structure with dependencies

---
+++ src/main/java/com/avtutov/FlightPing/FlightPingApplication.java
@@ -0,0 +1,13 @@
+package com.avtutov.FlightPing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightPingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightPingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/avtutov/FlightPing/FlightPingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avtutov.FlightPing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightPingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
